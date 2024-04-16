053552cf1caedf6ca07e1de9b544d00e1b0ada96
loisblin <blinlois@gmail.com>
2024-04-16 14:54:28 +0200

    str to json

--- read_data.py
+++ read_data.py
@@ -1,20 +1,23 @@
-#execute the sql file to create the database for postgresql
+import csv
+import json
 
-import psycopg2
-from psycopg2 import sql
-import pandas as pd
+def csv_to_json(csv_file, json_file):
+    # Ouvre le fichier CSV en mode lecture
+    with open(csv_file, 'r', encoding='utf-8') as file:
+        # Lit le fichier CSV avec le lecteur CSV
+        csv_reader = csv.DictReader(file)
+        # Initialise une liste pour stocker les lignes converties
+        data = []
+        # Parcourt chaque ligne dans le fichier CSV
+        for row in csv_reader:
+            # Ajoute la ligne convertie à la liste
+            data.append(row)
+    
+    # Écrit les données converties dans un fichier JSON
+    with open(json_file, 'w', encoding='utf-8') as file:
+        # Utilise json.dump() pour écrire les données dans le fichier JSON
+        json.dump(data, file, ensure_ascii=False, indent=4)
 
-#connect to the database
-conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="password")
-cur = conn.cursor()
-
-#execute the sql file
-f = open('create_database.sql', 'r')
-cur.execute(f.read())
-conn.commit()
-
-#close the connection
-cur.close()
-conn.close()
-
-print("Database created successfully")
+# Appel de la fonction avec le nom de votre fichier CSV en premier argument
+# et le nom du fichier JSON que vous souhaitez créer en second argument
+csv_to_json('proj831_data.csv', 'resultat.json')
